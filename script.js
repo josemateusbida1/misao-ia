877e392fa020045c4e6718de753f21c04f3a66a4
josemateusbida1 <126162145+josemateusbida1@users.noreply.github.com>
2024-06-27 11:25:09 -0300

    funcao mostra perguntas

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultados");
+
+const perguntas =[
+{
+    enunciado: "pergunta 1",
+    alternativas:["alternativas 1", "alternativa 2 "],
+    
+},
+{ }
+enunciado:
+
+    ]
