3aed724664ab6e9f352d0763d449655456a4c53b
josemateusbida1 <126162145+josemateusbida1@users.noreply.github.com>
2024-06-28 11:21:12 -0300

    botaaAlternativas

--- script.js
+++ script.js
@@ -7,10 +7,35 @@ const textoResultado = document.querySelector(".texto-resultados");
 const perguntas =[
 {
     enunciado: "pergunta 1",
-    alternativas:["alternativas 1", "alternativa 2 "],
-    
-},
-{ }
-enunciado:
+    alternativas:["alternativas 1", "alternativa 2 "],   
+  },
+  { 
+    enunciado:
+    alternatiavas:["alternativa 1", "alternativa 2 "]
+  },    
+  {
+    enunciado: "pergunta 3" ,
+    alternativas:["alternativa 1", " alternativa 2"],
+  };
+];
 
-    ]
+let atual = 0 ;
+let perguntaAtual ;
+
+function mostraPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativa();
+    
+}
+function mostraAlternativa(){
+  for (const alternativa of perguntaAtual.alternativas){
+      const botaoAlternatiavas = document . createElement("button")
+      botaoAlternativas . textContent = alternativa;
+      caixaAlternativas.appendChild(botaoAlternativas)
+  }
+}
+
+mostraPerguntas();
+
+
